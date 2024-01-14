2f993f3b60e2a758f5a25ccb6e806e1cfa087ee0
DBCOOPER <reddyindr53@gmail.com>
2024-01-14 11:57:09 +0530

    removed cors config

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 3000
 const path = require("path")
 const cors = require("cors")
 // Cors
-const corsOptions = {
- origin: process.env.ALLOWED_CLIENTS,
- // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
 
 // Default configuration looks like
 // {
